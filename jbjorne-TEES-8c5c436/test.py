10907655d750d755ec38080f00d96becad1b0e18
Nico Colic <ncolic@gmail.com>
2016-05-13 15:35:41 +0900

    first draft of servering

--- jbjorne-TEES-8c5c436/test.py
+++ jbjorne-TEES-8c5c436/test.py
@@ -14,17 +14,30 @@ from classify import classify , getModel
 import xml.etree.ElementTree as ET
 import json
 
+from flask import Flask, request , make_response , render_template
+
+app = Flask(__name__)
+
+
+@app.route('/tee_rest/', methods = ['GET','POST'])
+def rest():
+	result = mywrapper(request.form['text'])
+#	if 'text' in request.get_json():
+#		result = mywrapper(request.get_json()['text'])
+	return(result)
+
 def mywrapper(input_text):
-#	with open('working_file.txt','w') as f:
-#		f.write(input_text)
+	with open('working_file.txt','w') as f:
+		f.write(input_text)
 		
-#	myclassify('working_file.txt',"GE11",'output/output')
+	myclassify('working_file.txt','output/output')
 	
-#	os.remove('working_file.txt')
+	os.remove('working_file.txt')
 	
 	with gzip.open('output/output-pred.xml.gz') as f:
 		pa = tees_to_pubannotation(f.read())
 		print(pa)
+		return(pa)
 	
 def tees_to_pubannotation(input):
 	
@@ -57,24 +70,29 @@ def tees_to_pubannotation(input):
 		pre_json["relations"].append(relation_dict)		
 	return(json.dumps(pre_json,sort_keys=True,indent=4))
 
-def myclassify(input,model,output):
+def myclassify(input,output):
 	
 	# Define processing steps
 	selector, detectorSteps, omitDetectorSteps = getSteps(None, None, ["PREPROCESS", "CLASSIFY"])
 	
-	model = getModel(model)
-	preprocessor = Preprocessor()
+#	model = getModel(model)
+#	preprocessor = Preprocessor()
 	
 	classifyInput = preprocessor.process(input, (output + "-preprocessed.xml.gz"), None, model, [], fromStep=detectorSteps["PREPROCESS"], toStep=None, omitSteps=omitDetectorSteps["PREPROCESS"])
 			
-	detector = getDetector(None, model)[0]() # initialize detector object
+#	detector = getDetector(None, model)[0]() # initialize detector object
 	
-	detector.bioNLPSTParams = detector.getBioNLPSharedTaskParams(None, model)
+#	detector.bioNLPSTParams = detector.getBioNLPSharedTaskParams(None, model)
 	
 	detector.classify(classifyInput, model, output, fromStep=detectorSteps["CLASSIFY"], omitSteps=omitDetectorSteps["CLASSIFY"])
 	
 
 
+model = getModel("GE11")
+preprocessor = Preprocessor()
+detector = getDetector(None, model)[0]()
 
 if __name__=="__main__":
-	mywrapper("I saw the best minds of my generation destroyed by madness, starving hysterical naked, dragging themselves to the Negro streets at dawn looking for an angry fix.")
+#	mywrapper("I saw the best minds of my generation destroyed by madness, starving hysterical naked, dragging themselves to the Negro streets at dawn looking for an angry fix.")
+
+	app.run(debug=True)
